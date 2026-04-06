6defc11500f947d7a2866c329b02958dbbe60e79
Manoj_Chodisetti <chsivaji583@gmail.com>
2026-04-06 17:54:11 +0530

    Add home redirect and update chat mapping

--- src/main/java/com/chat/app/Controller/ChatController.java
+++ src/main/java/com/chat/app/Controller/ChatController.java
@@ -1,6 +1,5 @@
 package com.chat.app.Controller;
 
-
 import com.chat.app.model.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -9,14 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
+
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
     public ChatMessage sendMessage(ChatMessage message) {
         return message;
     }
 
-    @GetMapping("chat")
+    @GetMapping("/chat")
     public String chat() {
         return "chat";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/chat";
     }
 }
